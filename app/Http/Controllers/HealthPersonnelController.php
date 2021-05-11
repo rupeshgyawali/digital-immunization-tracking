9572bd62cde2080239bae275479efa8a711fa4ed
rupeshgyawali <rupeshgyawali49@gmail.com>
2021-05-11 10:12:27 +0545

    Create migration for health personal details, health personal api resource conroller and define basic actions

---
+++ app/Http/Controllers/HealthPersonnelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class HealthPersonnelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $health_personal = $request->only([
+            'name',
+            'email',
+            'phone_no',
+            'password'
+        ]);
+        $health_personal['password'] = Hash::make($health_personal['password']);
+        return User::create($health_personal);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->fill(
+            $request->only([
+                'name',
+                'email',
+                'phone_no'
+            ])
+        );
+
+        if ($user->isDirty()) {
+            $user->save();
+        }
+
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(["Delete Successfull"], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChildController;
 use App\Http\Controllers\ChildVaccineController;
+use App\Http\Controllers\HealthPersonnelController;
 use App\Http\Controllers\VaccineController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -39,4 +40,10 @@ Route::prefix('children/{child}')->name('children.vaccines.')->group(function ()
     Route::delete('/vaccines/{vaccine}', [ChildVaccineController::class, 'destroy'])->name('destroy')->missing(function () {
         return response()->json(['Resource Not Found'], 404);
     });
+});
+//Health Personnel using  User Model
+Route::apiResource('health-personnels', HealthPersonnelController::class)->parameters([
+    'health-personnels' => 'user',
+])->missing(function () {
+    return response()->json(['Health Personnel Not Found'], 404);
 });
